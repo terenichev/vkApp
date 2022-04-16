6e43fe6e38e3e9d45bc819732ae3347b7f408072
[denTerenichev] <[den.terenichev@mail.ru]>
2022-04-16 23:25:48 +0300

    fuuuuuuck

--- vkApp/Service/API/APIManager.swift
+++ vkApp/Service/API/APIManager.swift
@@ -9,11 +9,15 @@ import Foundation
 
 enum ApiType {
     
+   // case login   надо реализовать когда-то здесь
     case getFriends
     case getUserPhotos
     case getUserGroups
     case getSearchGroups
-    
-    
+//    
+//    var baseURL: URL{
+//        return URL(string: "https://api.vk.com/method/")!
+//    }
+
     
 }
